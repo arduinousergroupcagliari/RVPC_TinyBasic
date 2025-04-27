13fa2a927b5c6c1a92b429a534946eebfd675b38
Leandro Zaccaria <roglio@users.noreply.github.com>
2025-04-27 19:59:24 +0200

    First commit

---
+++ src/main.c
@@ -0,0 +1,62 @@
+/**
+ * This file is part of the RVPC project 
+ * https://github.com/OLIMEX/RVPC
+ * https://www.olimex.com/Products/Retro-Computers/RVPC/open-source-hardware
+ * 
+ * Copyright (c) 2024 Olimex Ltd
+ * Copyright (c) 2024 Curtis Whitley
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ch32v00x.h>
+
+#include "misc.h"
+#include "vga.h"
+#include "keyboard.h"
+#include "print.h"
+
+void basic(void);
+
+int main(void) {
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+	SystemCoreClockUpdate();
+	
+	// Disable GPIO Alternate Functions and external oscilator
+	// Othewize GPIO PORT A does not work
+	RCC_HSEConfig(RCC_HSE_OFF);
+	GPIO_PinRemapConfig(GPIO_Remap_PA1_2, DISABLE);
+
+	// Remapping
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+
+	vga_init();
+	kbd_init();
+
+	while (1) {
+		vga_cls();
+        basic();
+	}
+}
+
+void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void NMI_Handler(void) {
+	
+}
+
+void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast"))); 
+void HardFault_Handler(void) {
+	while (1) {
+		
+	}
+}
